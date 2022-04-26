6a49281f584c493bad0f92eacf3c77a10e142fed
AR <aki1013@naver.com>
2022-04-26 23:34:07 +0900

    인기페이지 구현 완료

--- src/main/java/site/metacoding/blogv3/domain/post/PostRepository.java
+++ src/main/java/site/metacoding/blogv3/domain/post/PostRepository.java
@@ -10,6 +10,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    // Love 기능 구현되면 쿼리 변경해야함.
+    @Query(value = "SELECT * FROM post ORDER BY id DESC LIMIT 0,9", nativeQuery = true)
+    List<Post> mFindByPopular();
 
     @Query(value = "SELECT * FROM post WHERE userId = :userId AND categoryId = :categoryId ORDER BY id DESC", nativeQuery = true)
     Page<Post> findByUserIdAndCategoryId(@Param("userId") Integer userId, @Param("categoryId") Integer categoryId,
